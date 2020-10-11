1edb52221d294ab9e2e2be35c7e23fc86d4ac00f
Alexander <alex.kononok@gmail.com>
2020-10-11 18:39:58 +0300

    added remove circle by tap

--- TheCreatorOfACircles/GameViewController.swift
+++ TheCreatorOfACircles/GameViewController.swift
@@ -12,46 +12,44 @@ class GameViewController: UIViewController {
   @IBOutlet weak var fieldForCirclesView: UIView!
   
   var circle: UIView!
-//  var count = 0
+  var arrCircles: [UIView] = []
+  
   let side: CGFloat = 100
   
   override func viewDidLoad() {
     super.viewDidLoad()
+    
     fieldForCirclesView.backgroundColor = .purple
     let gesture = UITapGestureRecognizer()
     gesture.addTarget(self, action: #selector(processTap(_:)))
     fieldForCirclesView.addGestureRecognizer(gesture)
-    
   }
   
   @objc func processTap(_ gesture: UITapGestureRecognizer) {
     let coord = gesture.location(in: fieldForCirclesView)
-    let x = coord.x
-    let y = coord.y
+    createCircle(x: coord.x, y: coord.y)
     
-    createCircle(x: x, y: y)
-//    if count < 3 {
-//      createCircle(x: x, y: y)
-//      count += 1
-//      print(count)
-//    } else {
-//      removeCircle(x: x, y: y)
-//      print("deleted")
-//    }
+    print(arrCircles)
+    print("============================================")
+    
+    var circleWhichWillRemoved: [UIView] = arrCircles.filter({ ($0.frame.minX < coord.x && coord.x < $0.frame.maxX && $0.frame.minY < coord.y && coord.y < $0.frame.maxY) })
+    print(circleWhichWillRemoved)
+    if circleWhichWillRemoved.count > 1 {
+      for circle in circleWhichWillRemoved {
+        circle.removeFromSuperview()
+      }
+      circleWhichWillRemoved = []
+    }
+    print("============================================")
   }
   
   func createCircle(x: CGFloat, y: CGFloat) {
     circle = UIView()
     circle.frame = CGRect(x: x - (side / 2), y: y - (side / 2), width: side, height: side)
     circle.layer.cornerRadius = circle.frame.width / 2
-//    circle.tag = Int.random(in: 0...Int.max)
-//    print(circle.tag)
     generateRandomColor()
     fieldForCirclesView.addSubview(circle)
-  }
-  
-  func removeCircle(x: CGFloat, y: CGFloat) {
-    circle.removeFromSuperview()
+    arrCircles.append(circle)
   }
   
   func generateRandomColor() {
